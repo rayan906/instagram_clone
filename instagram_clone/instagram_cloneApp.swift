7a6977f2a1449647e7b83a59ca8f9a175824a1e4
rayan906 <krayanahmed2004@gmail.com>
2026-02-23 10:54:18 +0530

    Initial Commit

---
+++ instagram_clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram_clone
+//
+//  Created by apprikart on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
